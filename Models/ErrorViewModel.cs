dea331db36d6b5e612fecb930f7cb23dee1a8878
Jakub Poręba <jakubpb17@gmail.com>
2026-01-14 00:07:37 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sklep_internetowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
